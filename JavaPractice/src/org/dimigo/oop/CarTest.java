ace3ef0844ed437fe510f10ad19422c3ed00b1a0
지현 <@Jihyun>
2015-04-16 12:26:59 +0900

    실습 과제 제출

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -20,15 +20,17 @@ public class CarTest {
 	
 	public static void main(String[] args) {
 		
-		Car car1=new Car();
-		Car car2=new Car();
-		Car car3=new Car();
+//		Car car1=new Car();
+//		Car car2=new Car();
+//		Car car3=new Car();
 		
-		car1.setCompany("현대자동차");
-		car1.setModel("제네시스");
-		car1.setColor("검정색");
-		car1.setMaxSpeed(225);
-		car1.setPrice(50000000);
+		Car2 car1=new Car2("현대자동차","제네시스","검정색",225,50000000);
+		
+//		car1.setCompany("현대자동차");
+//		car1.setModel("제네시스");
+//		car1.setColor("검정색");
+//		car1.setMaxSpeed(225);
+//		car1.setPrice(50000000);
 		
 		System.out.println("<< 자동차 목록 >>");
 		System.out.println("제조사명 : "+car1.getCompany());
@@ -38,11 +40,13 @@ public class CarTest {
 		System.out.println("가격 : "+String.format("%,d",car1.getPrice())+"원");
 		System.out.println("");
 		
-		car2.setCompany("기아자동차");
-		car2.setModel("K7");
-		car2.setColor("흰색");
-		car2.setMaxSpeed(246);
-		car2.setPrice(40000000);
+		Car2 car2=new Car2("기아자동차","K7","흰색",246,40000000);
+		
+//		car2.setCompany("기아자동차");
+//		car2.setModel("K7");
+//		car2.setColor("흰색");
+//		car2.setMaxSpeed(246);
+//		car2.setPrice(40000000);
 		
 		System.out.println("제조사명 : "+car2.getCompany());
 		System.out.println("모델명 : "+car2.getModel());
@@ -51,11 +55,13 @@ public class CarTest {
 		System.out.println("가격 : "+String.format("%,d",car2.getPrice())+"원");
 		System.out.println("");
 		
-		car3.setCompany("삼성자동차");
-		car3.setModel("SM7");
-		car3.setColor("회색");
-		car3.setMaxSpeed(200);
-		car3.setPrice(38000000);
+		Car2 car3=new Car2("삼성자동차","SM7","회색",200,38000000);
+		
+//		car3.setCompany("삼성자동차");
+//		car3.setModel("SM7");
+//		car3.setColor("회색");
+//		car3.setMaxSpeed(200);
+//		car3.setPrice(38000000);
 		
 		System.out.println("제조사명 : "+car3.getCompany());
 		System.out.println("모델명 : "+car3.getModel());
@@ -63,11 +69,6 @@ public class CarTest {
 		System.out.println("최대속도 : "+car3.getMaxSpeed()+"km");
 		System.out.println("가격 : "+String.format("%,d",car3.getPrice())+"원");
 		
-		
-		
-		
-		
-
 	}
 
 }
